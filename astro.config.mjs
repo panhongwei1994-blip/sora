8e586dea7ef22ebaedb093369262c4835caeca1f
panhongwei1994-blip <panhongwei1994@gmail.com>
2026-04-11 11:26:45 +0800

    Restore Worker config name to sora-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://ramen.panhongwei1994.workers.dev",
+  site: "https://sora-site.panhongwei1994.workers.dev",
   integrations: [vue(), sitemap()],
   adapter: cloudflare(),
   output: "server",
